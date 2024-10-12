246953f67a33a64744f36a58fa244292caa6a7e1
SulemJ <sulemjibrildawud@gmail.com>
2024-10-12 19:45:43 +0300

    update home

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,9 +5,9 @@ function Contact(params) {
 
     return <div  id="contact" class=" container">
          <p  class="center p-5 pb-3 " >Get in touch</p>
-        <div class="  py-12 m-5 pb-5 "> 
+        <div class=" contact  pb-5 "> 
             
-             <ul class="nav col-md-10 col-sm-4 list-unstyled  d-flex up center">
+             <ul class=" col-md-12 col-sm-12 list-unstyled  d-flex up ">
       <li class="ms-3 up "><a class="up text-body-secondary"  target="_blank" href="https://www.linkedin.com/in/sulem-jibril-dawud-b04252220"><img class="bi up " src="pics\linkedin.svg"></img></a></li>
       <li class="ms-3 up "><a class="up text-body-secondary" target="_blank" href="https://github.com/SulemJ"><img class="bi up " src="pics\github.svg"></img></a></li>
       <li class="ms-3 up"><a class="up text-body-secondary" target="_blank" href="https://t.me/HBMme"><img class="bi up " src="pics\telegram.svg" ></img></a></li>
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,12 @@ import React from "react";
  function Home(params) {
     return <div id="home" class=" container">
         <div class=" home row">
-            <div class=" col-md-5 col-xl-6 col-sm-12 name">
+            <div class=" col-md-5 col-xl-5 col-sm-6  name">
                <p>Hello there! 👋  </p>
                <p> I'm Sulem Jibril Dawud</p>
                <p>Developer and Designer</p>
             </div>
-            <div class=" col-md-5 col-xl-5 col-sm-12">
+            <div class=" col-md-5 col-xl-5 col-sm-6  ">
                 <img src="pics/bg.png" alt="" class="mt-1 bg" />
             </div>
         </div>
